944c37cc7761213f07cdd8a4cc527bb3bf0af673
Byron <byronwmit@gmail.com>
2022-05-17 13:57:53 +0100

    Adding my first cypress tests
    These are running against the "production" version of the site (next steps would be tie this into CI/CD)

--- cypress/integration/smokeTest.js
+++ cypress/integration/smokeTest.js
@@ -4,3 +4,17 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+describe('My First Passing Test', () => {
+    it('Visit my first site', () => {
+      cy.visit('https://main.dlke5sighv45.amplifyapp.com/')
+      cy.contains(`My First Paragraph`)
+    })
+  })
+
+  describe('My First Failing Test', () => {
+    it('Visit my first site', () => {
+      cy.visit('https://main.dlke5sighv45.amplifyapp.com/')
+      cy.contains(`My Only Paragraph`)
+    })
+  })
